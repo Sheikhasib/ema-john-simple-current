f4e3f24a832be190a9b439619544881eefb91530
Sheikhasib <sheikhhasib037@gmail.com>
2024-01-17 12:05:20 +0600

    adeed cart and calculation

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css';
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCart = (product) => {
+        // cart.push(product);
+        const newCart = [...cart, product]; // using spread operator(...) to put the previous item to the cart
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
@@ -16,11 +25,12 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToCart={handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
