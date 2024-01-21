36b41467f3b6b079e3a56121cdab6db0dd814121
Sheikhasib <sheikhhasib037@gmail.com>
2024-01-21 11:58:02 +0600

    product quantity added to the cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -35,7 +35,20 @@ const Shop = () => {
 
     const handleAddToCart = (product) => {
         // cart.push(product);
-        const newCart = [...cart, product]; // using spread operator(...) to put the previous item to the cart
+        let newCart = [];
+        // const newCart = [...cart, product]; // using spread operator(...) to put the previous item to the cart
+        // if product does not exist in the cart, then set quantity =1 
+        // if exist update by 1
+        const exists = cart.find(pd => pd.id === product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }
+        else{
+            exists.quantity = exists.quantity + 1;
+            const remaining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...remaining, exists];
+        }
         setCart(newCart);
         addToDb(product.id);
     }
